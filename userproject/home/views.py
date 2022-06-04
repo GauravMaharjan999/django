b20c652c2aefef04269b53a7ff6a9acba6b979d8
Gaurav Maharjan <rijanmaharjan999@gmail.com>
2022-06-04 13:30:36 +0545

    change

---
+++ userproject/home/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
+#password for test user islogout()   rijan@999
+# Create your views here.
+def index(request):
+   print(request.user)
+   if request.user.is_anonymous:
+      return render('/login')
+   return render(request,'index.html')
+
+def login(request):
+   return render(request,'login.html')
+   print("The request method is----------",request.method)
+   if request.method =="POST":
+      username = request.POST.get('username')
+      password = request.POST.get('password')
+      
+
+      #check if user has entered correct credentials 
+      user = authenticate(username=username, password=password)
+
+      if user is not None:
+    # A backend authenticated the credentials
+         login(request,user)
+         return redirect('/')
+      else:
+    # No backend authenticated the credentials
+         return redirect(request,'login.html')   
+         
+   return redirect(request,'login.html')   
+
+   # return render(request,"login.html")
+
+   
+
+
+
+def logoutUser(request):
+   logout(request)
+   return redirect('/login')
+    
